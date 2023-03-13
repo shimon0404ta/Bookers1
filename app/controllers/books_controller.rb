3c8a191a0b29ceb787ba6cba41c8d64f4a5cc695
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-03-13 06:22:58 +0000

    [Add] 3-13 課題２：投稿機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+   book = Book.new(book_params)
+   book.save
+   redirect_to '/books/show'
   end
 
   def index
   end
 
   def show
+   @book = Book.new
   end
 
   def edit
+  end
+
+
+  private
+
+  def book_params
+    #params.require(:book).
+    permit(:title, :body)
   end
 end
