9fb4ae289c87bec89d1f4e240fe64a123fa75d75
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-03-14 03:38:32 +0000

    [Finish] 3-13 課題２：調整完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,47 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @books = Book.all
     @book = Book.new
   end
 
-  def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to '/books/show'
-  end
-
-  def index
-  end
-
   def show
-   @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      @books = book.all
+      render action: edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notiice: 'Book was successfully destroyed.'
   end
 
 
   private
 
   def book_params
-    #params.require(:book).
-    permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
